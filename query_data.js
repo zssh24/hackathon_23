65ec011ffdbf92c6392945a01de565d8706be928
zssh24 <zain@sh24.org.uk>
2023-12-14 10:57:03 +0000

    got query_data working to produce total_products

--- query_data.js
+++ query_data.js
@@ -1,4 +1,5 @@
 const { DBSQLClient } = require('@databricks/sql');
+var fs = require('fs/promises');
 
 const token = 'dapi3509b026e18ba37790d3598c5007d9b5';
 const serverHostname = 'dbc-25dae16e-9b76.cloud.databricks.com';
@@ -15,8 +16,6 @@ const queries = [
 
   "SELECT local_authority_district_name AS District, COUNT(*) AS `Number of products` FROM prod.warehouse.sales_events INNER JOIN prod.warehouse.delivery_addresses ON sales_events.delivery_address_sk = delivery_addresses.sk WHERE product_type <> 'STI Test Result Set' GROUP BY 1 ORDER BY District"
 ]
-
-
 
 const client = new DBSQLClient();
 const connectOptions = {
@@ -46,9 +45,9 @@ const executeQueries = async () => {
         console.log(`Results for Query: ${query}`);
         console.table(result);
 
-        // Save results to a JSON file
+        // Save results to a JSON file asynchronously using fs.promises
         const jsonResults = JSON.stringify(result, null, 2);
-        fs.writeFileSync('total_products.json', jsonResults);
+        await fs.writeFile('data/total_products.json', jsonResults);
       } catch (error) {
         console.log(`Error executing query: ${query}`, error);
       } finally {
